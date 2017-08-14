b2fc9cba775da9918a41a1ed8f0727ed4c138826
Kimon Fotiadis <kimonfotiadis@Kimons-MacBook-Pro.local>
2017-08-14 16:13:55 +0200

    Initial Commit

---
+++ BTS III/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BTS III
+//
+//  Created by Kimon Fotiadis on 8/14/17.
+//  Copyright © 2017 Kimon Fotiadis. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
